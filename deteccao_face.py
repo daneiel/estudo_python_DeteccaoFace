a2edafb39bcd1e3bc0db8e763e275017e7ff97df
DANIEL SOUZA <daniel.oliveira@conductor.com.br>
2019-02-06 14:51:32 -0200

    Deteccao de face com borderbox

--- deteccao_face.py
+++ deteccao_face.py
@@ -1,2 +1,20 @@
 import cv2
 
+imagem = cv2.imread('pessoas.jpg')
+
+classificador = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+deteccoes = classificador.detectMultiScale(imagemCinza)
+
+print(deteccoes)
+print(len(deteccoes))
+
+for (x, y, l, a) in deteccoes:
+    cv2.rectangle(imagem, (x, y), (x + l, y + a), (0,255,0), 2)
+
+cv2.imshow('Detector de faces', imagem)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
